673be1430e2447fc9b26ab62fa232f97511484bc
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-07-16 12:58:35 +0300

    tests created

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  describe 'associations' do
+    it { should belong_to :question }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :body }
+    it { should validate_presence_of :correct }  
+  end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  describe 'associations' do
+    it { should have_many :answers }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }  
+  end
+  
+end
