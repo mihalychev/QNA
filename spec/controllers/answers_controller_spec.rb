1b4b93ec9c35775009b56d040fbff46ecacafc72
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-07-18 00:25:39 +0300

    answer new test complete

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  
+  describe 'GET #new' do
+    let(:question) { create(:question) }
+    
+    it 'renders new view' do
+      get :new, params: { question_id: question }
+      expect(response).to render_template :new
+    end
+  end
 end
