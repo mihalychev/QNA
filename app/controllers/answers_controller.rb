18bb3973be1ce7e627956cd206a67a9faf5da5d1
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-07-18 00:34:20 +0300

    answer edit test complete

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,17 @@
 class AnswersController < ApplicationController
   def new; end
+
+  def edit; end
+
+  private
+
+  def answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
+  end
+
+  helper_method :answer
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #new' do
-    let(:question) { create(:question) }
-    
+
     it 'renders new view' do
       get :new, params: { question_id: question }
       expect(response).to render_template :new
     end
   end
+
+  describe 'GET #edit' do
+    let(:answer) { create(:answer, question: question) }
+
+    it 'renders edit view' do
+      get :edit, params: { id: answer, question_id: question }
+      expect(response).to render_template :edit
+    end
+  end
+
 end
