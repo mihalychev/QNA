20c79a4e3e52c9abcae1515e8e921aa8b95f3e1a
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-07-22 17:36:03 +0300

    view test complete

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,9 +1,18 @@
 FactoryBot.define do
+  sequence :body do |n|
+    "body: #{n}"
+  end
+
   factory :answer do
     body { "MyText" }
 
     trait :invalid do
       body { nil }
     end
+  end
+
+  factory :rand_body_answer, class: Answer do
+    body
+    question
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
+  sequence :title do |n|
+    "title: #{n}"
+  end
+
   factory :question do
     title { "MyString" }
     body { "MyText" }
@@ -6,5 +10,10 @@ FactoryBot.define do
     trait :invalid do
       title { nil }
     end
+  end
+
+  factory :rand_title_question, class: Question do
+    title
+    body { "MyTesx" }
   end
 end
