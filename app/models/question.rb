9e5498ffeb3d394a0107395233455a0eb873eb14
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-08-10 17:18:29 +0300

    attach one file to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   
   has_many :answers, dependent: :destroy
+  has_one_attached :file
 
   validates :title, presence: true
   validates :body, presence: true
