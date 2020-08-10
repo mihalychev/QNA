9d589a342b2a1c6c65d061eb1c4ff9ee80185cd8
Maxim Mihalichev <maxim.myhalichev@yandex.ru>
2020-08-10 17:58:47 +0300

    attach many files to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,8 @@ class Question < ApplicationRecord
   belongs_to :user
   
   has_many :answers, dependent: :destroy
-  has_one_attached :file
+  
+  has_many_attached :files
 
   validates :title, presence: true
   validates :body, presence: true
--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -34,10 +34,11 @@ feature 'User can create question', %q{
     scenario 'asks a question with attached file' do
       fill_in 'Title', with: 'Title'
       fill_in 'Body', with: 'Body'
-      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
       click_on 'Ask'
 
       expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   
     scenario 'asks a question with errors' do
