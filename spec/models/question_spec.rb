0e66a06ee3cc691a5ab3f5bbca76d4f403703cef
Maxim Mikhalychev <m.mikhalychev@sftpro.ru>
2021-05-19 22:44:10 +0300

    more question spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -25,4 +25,38 @@ RSpec.describe Question, type: :model do
   it 'has many attached files' do
     expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
+
+  describe '#with_best_answer?' do
+    let(:question) { create :question }
+    let(:answer) { create :answer, best: true }
+    let(:question_with_best_answer) { create :question, answers: [answer] }
+
+    context 'without best answer' do
+      it 'returns false' do
+        expect(question_with_best_answer.with_best_answer?).to eq true
+      end
+    end
+
+    context 'with best answer' do
+      it 'returns true' do
+        expect(question.with_best_answer?).to eq false
+      end
+    end
+  end
+
+  describe '#created_time' do
+    let!(:question) { create :question, created_at: Time.parse('2021-08-1T12:00:00Z') }
+
+    it 'returns created time with format hh:mm' do
+      expect(question.created_time).to eq '12:00'
+    end
+  end
+
+  describe '#created_date' do
+    let(:question) { create :question, created_at: Time.parse('2021-08-1T12:00:00Z') }
+
+    it 'returns created date with format dd:mm:yy' do
+      expect(question.created_date).to eq '01.08.2021'
+    end
+  end
 end
